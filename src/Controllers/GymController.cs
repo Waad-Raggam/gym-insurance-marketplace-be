3a263fc9b328d1c940c3366398ea9256419487de
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-10-29 21:21:49 +0300

    #51 - Add POST /gym endpoint

---
+++ src/Controllers/GymController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using src.DTO;
+using src.Entity;
+using src.Services;
+using src.Services.Gym;
+using static src.DTO.GymDTO;
+
+namespace src.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class GymController : ControllerBase
+    {
+        private readonly IGymService _gymService;
+
+        public GymController(IGymService gymService)
+        {
+            _gymService = gymService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GymReadDto>> CreateOne([FromBody] GymCreateDto createDto)
+        {
+            // // exact user information by token
+            // var authenticateClaims = HttpContext.User;
+            // // get user id by claims
+            // var UserId = authenticateClaims
+            //     .FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!
+            //     .Value;
+            // // string => Guid
+            // var userGuid = new Guid(UserId);
+
+            var gymCreated = await _gymService.CreateOneAsync( createDto);
+
+            if (gymCreated == null)
+            {
+                return Conflict("Gym creation failed.");
+            }
+
+            return Ok(gymCreated);
+        }
+    }
+}
