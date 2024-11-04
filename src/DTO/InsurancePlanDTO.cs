a8d0a70a295218e19a6643ee39a529a99c3a581e
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-05 00:35:26 +0300

    #3 - Add plan description to insurance entity

--- src/DTO/InsurancePlanDTO.cs
+++ src/DTO/InsurancePlanDTO.cs
@@ -12,6 +12,7 @@ namespace src.DTO
             public string PlanName { get; set; }
             public decimal MonthlyPremium { get; private set; }
             public string CoverageType { get; private set; }
+            public string PlanDescription { get; private set; }
             public List<string> CoverageDetails { get; private set; }
         }
     }
--- src/Database/DatabaseContext.cs
+++ src/Database/DatabaseContext.cs
@@ -48,9 +48,9 @@ namespace src.Database
             modelBuilder.HasPostgresEnum<Role>();
 
             modelBuilder.Entity<InsurancePlan>().HasData(
-        new InsurancePlan(1, "Basic Plan", 30m, "Basic Coverage", new List<string> { "General Liability Insurance", "Workers’ Compensation Insurance" }),
-        new InsurancePlan(2, "Standard Plan", 50m, "Comprehensive Coverage", new List<string> { "General Liability", "Workers’ Compensation", "Professional Liability" }),
-        new InsurancePlan(3, "Premium Plan", 80m, "Premium Comprehensive", new List<string> { "General Liability", "Workers’ Compensation", "Professional Liability", "Business interruption insurance", "Commercial property insurance" })
+        new InsurancePlan(1, "GymBasic", 30m, "Basic Coverage", "Designed to cover essential liabilities, offering peace of mind for everyday operations so you can focus on training.", new List<string> { "General Liability Insurance", "Workers’ Compensation Insurance" }),
+        new InsurancePlan(2, "GymStandard", 50m, "Comprehensive Coverage", "Provides comprehensive liability coverage for both general and professional risks, plus options to protect your equipment and more for a safer gym environment.", new List<string> { "General Liability", "Workers’ Compensation", "Professional Liability" }),
+        new InsurancePlan(3, "GymElite", 80m, "Premium Comprehensive", "Our highest level of protection, covering liability, equipment breakdown, and business interruptions, ensuring your gym is safeguarded from all angles for continuous peace of mind.", new List<string> { "General Liability", "Workers’ Compensation", "Professional Liability", "Business interruption insurance", "Commercial property insurance" })
     );
 
             modelBuilder.Entity<GymInsurance>()
